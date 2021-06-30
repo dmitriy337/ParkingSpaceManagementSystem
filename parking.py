7ed34267c507e41cef5e53466fdf0e7db062d12e
Vuhiza <47759698+dmitriy337@users.noreply.github.com>
2021-06-30 12:01:39 +0300

    Add some methods to ParkingLot

--- parking.py
+++ parking.py
@@ -1,12 +1,25 @@
 
-
 class ParkingLotsTypes():
     BIKE_LOT = 1
     COMPACT_LOT = 2
     BIG_LOT = 3
+    
 
 class ParkingLot():
-    pass
+    type: int
+    isOccupied: bool
+
+    def __init__(self, type) -> None:
+        self.type = type
+        self.isOccupied = False
+    
+    def MarkAsOccupied(self):
+        self.isOccupied = True
+        
+    def MarkAsUnoccupied(self):
+        self.isOccupied = False
+
 
 class Parking():
     pass
+
