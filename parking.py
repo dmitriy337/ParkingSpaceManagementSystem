17a439e8f4737feb51c4b5f2dab101b30729541a
Vuhiza <47759698+dmitriy337@users.noreply.github.com>
2021-06-30 12:00:36 +0300

    Init commit

---
+++ parking.py
@@ -0,0 +1,12 @@
+
+
+class ParkingLotsTypes():
+    BIKE_LOT = 1
+    COMPACT_LOT = 2
+    BIG_LOT = 3
+
+class ParkingLot():
+    pass
+
+class Parking():
+    pass
---
+++ vehicle.py
@@ -0,0 +1,11 @@
+class Vehicle():
+    pass
+
+class Bike(Vehicle):
+    pass
+
+class Car(Vehicle):
+    pass
+
+class Bus(Vehicle):
+    pass
